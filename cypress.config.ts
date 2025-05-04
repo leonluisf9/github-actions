4b6d9735242d6b4ece8d6a05d99d758e46c711ad
Maverick <leonluisf9@outlook.com>
2025-05-04 10:09:04 -0500

    add changes to yaml

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
-    port: 5173,
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
-      viteConfig,
+      framework: "react",
+      bundler: "vite",
     },
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
